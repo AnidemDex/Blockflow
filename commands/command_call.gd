0720c5eece57e494f407da87d4f81c968424478a
AnidemDex <dav.medrin@gmail.com>
2023-03-18 21:42:37 -0500

    Add command call.
    Base script. No functionality added yet.

---
+++ commands/command_call.gd
@@ -0,0 +1,12 @@
+extends Command
+
+func _execution_steps(manager) -> void:
+	pass
+
+
+func _get_command_name() -> String:
+	return "Call"
+
+
+func _get_command_icon() -> Texture:
+	return load("res://icon.svg")
