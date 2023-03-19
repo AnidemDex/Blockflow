c68e6ec61b670630ce328e3f56074e5f049c590e
AnidemDex <dav.medrin@gmail.com>
2023-03-18 21:49:36 -0500

    Add condition command.
    Adds condition command base script. No functionality added yet.
    
    Between us (nobody reads commit history), I have no idea how I'm gonna
    implement this.

---
+++ commands/command_condition.gd
@@ -0,0 +1,13 @@
+extends Command
+
+func _execution_steps(manager) -> void:
+	pass
+
+
+func _get_command_name() -> String:
+	return "Condition"
+
+
+func _get_command_icon() -> Texture:
+	return load("res://icon.svg")
+
