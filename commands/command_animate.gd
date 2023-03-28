3fa044c77a892ba06f9516fcab89e6776f793a42
Crystalwarrior <Varsash@Gmail.com>
2023-03-28 15:10:34 +0300

    add an "animate" command which will later interface with Godot's animation player(s) (#4)
    * add an "animate" command which will later interface with Godot's animation player(s)
    
    * load the animate command

---
+++ commands/command_animate.gd
@@ -0,0 +1,12 @@
+extends Command
+
+func _execution_steps(manager) -> void:
+	pass
+
+
+func _get_command_name() -> String:
+	return "Animate"
+
+
+func _get_command_icon() -> Texture:
+	return load("res://addons/blockflow/icons/animation.svg")
