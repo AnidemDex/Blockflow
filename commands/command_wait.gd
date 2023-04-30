3d6e27b71f394495e1c03f9b8b87f5cc5b18192c
Alex Noir <Varsash@gmail.com>
2023-04-30 05:17:33 +0300

    Fix wait, call cmds not updating visually when edited in inspector

--- commands/command_wait.gd
+++ commands/command_wait.gd
@@ -1,7 +1,12 @@
 @tool
 extends Command
 
-@export var wait_time:float
+@export var wait_time:float:
+	set(value):
+		wait_time = value
+		emit_changed()
+	get: return wait_time
+
 
 func _execution_steps() -> void:
 	command_started.emit()
@@ -11,6 +16,14 @@ func _execution_steps() -> void:
 
 func _get_name() -> String:
 	return "Wait"
+
+
+func _get_hint() -> String:
+	var hint_str = str(wait_time) + " second"
+	# pluralize if we're not at '1 second'
+	if wait_time != 1:
+		hint_str += "s"
+	return hint_str
 
 
 func _get_icon() -> Texture:
