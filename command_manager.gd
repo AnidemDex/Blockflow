AnidemDex/Blockflow

commit 88e5aaf34da90b7d453ee3e49f781007943b29ee

Fix an error in go_to_command

command_manager.gd
@@ -95,6 +95,11 @@ func go_to_command(command_idx:int, timeline:Timeline=null) -> void:
 		assert(false, str(self)+"::go_to_command: Trying to use an unexisting timeline!")
 		return
 	
+	# Prevents an error and ends the timeline if there are no more commands
+	if current_timeline.commands.size() >= command_idx:
+		_notify_timeline_end()
+		return
+	
 	current_command = current_timeline.get_command(command_idx)
 	current_command_idx = command_idx
 	

commit 4d7fc95957463ab9a40b0cb4eb7bacdbd28c93b6

Fix the error introduced by 88e5aaf

command_manager.gd
@@ -96,7 +96,7 @@ func go_to_command(command_idx:int, timeline:Timeline=null) -> void:
 		return
 	
 	# Prevents an error and ends the timeline if there are no more commands
-	if current_timeline.commands.size() >= command_idx:
+	if command_idx >= current_timeline.commands.size():
 		_notify_timeline_end()
 		return
 	
@@ -235,7 +235,7 @@ func _on_command_started(command:Command) -> void:
 func _on_command_finished(command:Command) -> void:
 	command_finished.emit(command)
 	if command.continue_at_end:
-		go_to_next_command()
+		go_to_next_command.call_deferred()
 
 
 func _notify_timeline_start() -> void: