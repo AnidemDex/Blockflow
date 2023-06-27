3ea3ee47df0a38a52698c9254c1fdbcce26a23da
Alex Noir <Varsash@gmail.com>
2023-06-27 19:40:44 +0300

    Split "NO_RETURN" behavior into its own "End Timeline" command Implement "repeat" and "next" behaviors for the return command, expose these settings to the user Add a hint string to the return command Add an option for the Return command to return to last timeline rather than last goto (though it'll still resume from the last timeline's jump)

--- commands/command_return.gd
+++ commands/command_return.gd
@@ -1,18 +1,40 @@
 @tool
 extends Command
 
-enum ReturnValue {BEFORE=-1,NO_RETURN, AFTER}
+@export_enum("Repeat:0", "Next:1") var behavior: int = 1:
+	set(value):
+		behavior = value
+		emit_changed()
+	get:
+		return behavior
 
-var returns_to:int = ReturnValue.AFTER
+
+@export var to_last_timeline:bool = false:
+	set(value):
+		to_last_timeline = value
+		emit_changed()
+	get:
+		return to_last_timeline
+
 
 func _execution_steps() -> void:
 	command_started.emit()
-	command_manager.return_command(returns_to)
+	command_manager.return_command(behavior, to_last_timeline)
 
 
 func _get_name() -> String:
 	return "Return"
 
 
+func _get_hint() -> String:
+	return ("to last timeline" if to_last_timeline else "to last jump") +\
+		   (" (repeat)" if behavior == 0 else "")
+
+
 func _get_icon() -> Texture:
 	return load("res://addons/blockflow/icons/return.svg")
+
+
+func _init() -> void:
+	super()
+	continue_at_end = false
