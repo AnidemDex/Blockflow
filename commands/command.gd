fc103b7ef3bf34074741b86b93a0cf92cada49c6
AnidemDex <anidemdex@github.com>
2023-09-17 14:53:01 -0500

    Start restructuring commands.
    This is part of the command restructure to make them able to hold subcommands.

--- commands/command.gd
+++ commands/command.gd
@@ -50,6 +50,62 @@ var execution_steps:Callable = _execution_steps
 ## should not be assigned manually.
 var command_manager:Node
 
+## The command name. Use [method _get_name] to define the name. [br]
+## Command name is used by editor, it'll displayed in item the first column
+## next to command icon.
+## The command name is also used when editor is creating command buttons in
+## editor.
+## [codeblock]
+## func _get_name() -> StringName:
+##     return &"Example Command"
+## [/codeblock]
+var command_name:StringName :
+	set(value): return
+	get: return _get_name()
+
+## Comand icon displayed in editor. 
+## [br]
+## Use [method _get_icon] to define the icon. It'll be displayed in item first column.
+## [codeblock]
+## func _get_icon() -> Texture:
+##     return load("res://icon.svg")
+## [/codeblock]
+var command_icon:Texture :
+	set(value): return
+	get: return _get_icon()
+
+## Command hint. Use [method _get_hint] to define the command hint. [br]
+## It will be displayed on command item middle column, 
+## useful to preview values previously set in inspector.
+## [codeblock]
+## func _get_hint() -> String:
+##     return "Hi, I'm command %s" % command_name
+## [/codeblock]
+var command_hint:String :
+	set(value): return
+	get: return _get_hint()
+
+## Command hint icon. Use [method _get_hint_icon] to define the texture. [br]
+## This texture will be displayed before [command_hint].
+## [codeblock]
+## func _get_hint_icon() -> Texture:
+##     return load("res://icon.svg")
+## [/codeblock]
+var command_hint_icon:Texture :
+	set(value): return
+	get: return _get_hint_icon()
+
+## Command description is used by editor and will be show as tooltip hint. [br]
+## Use [method _get_description] to define the description.
+## [codeblock]
+## func _get_description() -> String:
+##     return "This is an example command"
+## [/codeblock]
+var command_description:String :
+	set(value): return
+	get: return _get_description()
+
+
 ## Target node that [member target] points to. This value is assigned by
 ## [member command_manager] before command execution if [member target] is a
 ## valid path, else node assigned in
@@ -72,28 +128,6 @@ var branch_names:PackedStringArray:
 # A [WeakRef] that points to the command owner of this command
 var group_owner:WeakRef
 
-
-## Returns this command name.
-func get_command_name() -> String:
-	return _get_name()
-
-## Returns this comand icon.
-func get_icon() -> Texture:
-	return _get_icon()
-
-## Returns this command hint.
-func get_hint() -> String:
-	return _get_hint()
-
-## Returns this command hint icon.
-func get_hint_icon() -> Texture:
-	return _get_hint_icon()
-
-## Returns this command description.
-func get_description() -> String:
-	return _get_description()
-
-
 ## Defines the execution behaviour of this command.
 ## This function is the default value of [member execution_steps]
 ## and you should override it if you are defining the command in a
@@ -115,37 +149,19 @@ func _execution_steps() -> void:
 	assert(false, "_execution_steps")
 
 
-## Returns this comman name.
-## Command name is used by editor, it'll displayed in item the first column
-## next to command icon.
-## The command name is also used when editor is creating command buttons in
-## editor.
 func _get_name() -> StringName:
 	assert(!resource_name.is_empty(), "_get_name()")
 	return "UNKNOW_COMMAND" if resource_name.is_empty() else resource_name
 
-
-## Returns this command icon.
-## Command icon is used by editor. It'll be displayed in item first column.
 func _get_icon() -> Texture:
 	return null
 
-
-## Returns this command hint. 
-## The returned string will be displayed
-## on command item middle column, useful to preview values previously set
-## in inspector.
 func _get_hint() -> String:
 	return ""
 
-
-## Returns this command hint icon.
-## The returned texture will be displayed before command hint.
 func _get_hint_icon() -> Texture:
 	return null
 
-## Returns this command description.
-## Command description is used by editor and will be show as tooltip hint.
 func _get_description() -> String:
 	return ""
 
@@ -153,7 +169,7 @@ func _get_branch_names() -> PackedStringArray:
 	return []
 
 func _to_string() -> String:
-	return "<Command [%s:%s] #%s>" % [get_command_name(),index,get_instance_id()]
+	return "<Command [%s:%s] #%s>" % [command_name,index,get_instance_id()]
 
 func _init() -> void:
 	resource_local_to_scene = true
