565d0f5d09b8dc2b3629989983c7d84c2456d5f4
AnidemDex <anidemdex@github.com>
2023-10-08 21:36:03 -0500

    Replace Timeline with CommandCollection.
    - Renames CommandCollection to Collection
    - Renames Timeline with CommandCollection.
    
    This is made this way due someone (crystalwarrior) using the plugin on alpha state
    so their work is not lost during the class transition.

--- command_collection.gd
+++ command_collection.gd
@@ -1,144 +1,82 @@
 @tool
-extends Resource
+extends "res://addons/blockflow/collection.gd"
 class_name CommandCollection
 
-## Collection of commands.
 ##
-## CommandCollection acts as an array with extra functions
-## to interact with the array easily.
+## This resource only keeps an ordered reference of all commands registered on it.
+##
 
-## [WeakRef] owner of this collection.
-## [br][method weak_owner.get_ref] value can be:
-## [br]  - A [Timeline]
-## [br]  - A [Command], meaning this command is a subcommand of that command.
-## [br]  - A [code]null[/code] value, meaning it doesn't has an owner or the
-## owner failed setting its own reference.
-var weak_owner:WeakRef
+var _bookmarks:Dictionary = {}
+var _command_list:Array = []
+var _branches:Array = []
+var _subcommads:Array = []
 
-var collection:Array[Command] = []:
-	set(value):
-		collection = value
-		for c in value:
-			_update_command_owner(c)
-		emit_changed()
-	get:
-		return collection
+func get_command(position:int) -> Command:
+	if position >= _command_list.size():
+		push_error("position >= _command_list.size()")
+		return null
+	return _command_list[position]
 
-func add(command:Command) -> void:
-	if has(command):
-		push_error("Trying to add an command to the collection, but the command is already added")
-		return
-	collection.append(command)
-	_update_command_owner(command)
+## Get the command [code]position[/code] from its [code]bookmark[/code]
+func get_command_by_bookmark(bookmark:StringName) -> Resource:
+	if not bookmark in _bookmarks:
+		push_error("get_command_by_bookmark: Couldn't find command with a bookmark: ", bookmark)
+	
+	return _bookmarks.get(bookmark, null)
+
+## Returns the command position in the timeline.
+func get_command_absolute_position(command) -> int:
+	return _command_list.find(command)
+
+func get_command_count() -> int:
+	return _command_list.size()
+
+func is_branch(command:Command) -> bool:
+	return command in _branches
+
+func is_subcommand(command:Command) -> bool:
+	return command in _subcommads
+
+## Forces an update to contained data.
+## [br]This makes all the contained data:
+## [br]- Update their index.
+## [br]- Update their bookmars.
+## [br]- Update their owners.
+## [br]- Update their structure.
+func update() -> void:
+	_notify_changed()
+
+var _index:int
+func _update_data(from_collection:Array):
+	if from_collection.is_empty(): return
+	
+	for command in from_collection:
+		_command_list.append(command)
+		command.index = _index
+		_index += 1
+		# branches goes first
+		if command.branches:
+			_update_data(command.branches.collection)
+			_branches.append_array(command.branches.collection)
+		
+		if command.commands:
+			_update_data(command.commands.collection)
+			_subcommads.append_array(command.commands.collection)
+		
+		if not command.bookmark.is_empty():
+			_bookmarks[command.bookmark] = command
+		
+		command.weak_collection = weakref(self)
+
+
+var _updating_data:bool = false
+func _notify_changed() -> void:
+	if _updating_data: return
+	_updating_data = true
+	_index = 0
+	_command_list = []
+	_update_data(collection)
+	_updating_data = false
 	emit_changed()
 
-func insert(command:Command, at_position:int) -> void:
-	if has(command):
-		push_error("Trying to add an command to the collection, but the command already exist")
-		return
-	
-	var idx = at_position if at_position > -1 else collection.size()
-	collection.insert(idx, command)
-	_update_command_owner(command)
-	emit_changed()
-
-# can't use duplicate lmao
-func copy(command:Command, to_position:int) -> void:
-	var duplicated = command.duplicate()
-	var idx = to_position if to_position > -1 else collection.size()
-	collection.insert(idx, duplicated)
-	_update_command_owner(command)
-	emit_changed()
-
-func move(command:Command, to_position:int) -> void:
-	if !has(command):
-		push_error("Trying to move an command in the collection, but the command is not added.")
-		return
-	
-	var old_position:int = get_command_position(command)
-	if old_position < 0:
-		return
-	
-	to_position = to_position if to_position > -1 else collection.size()
-	if to_position == old_position:
-		emit_changed()
-		return
-	
-	collection.remove_at(old_position)
-	
-	if to_position < 0 or to_position > collection.size():
-		to_position = collection.size()
-	
-	collection.insert(to_position, command)
-	_update_command_owner(command)
-	emit_changed()
-
-func erase(command) -> void:
-	collection.erase(command)
-	emit_changed()
-
-func remove(position:int) -> void:
-	collection.remove_at(position)
-	emit_changed()
-
-func clear() -> void:
-	collection.clear()
-	emit_changed()
-
-## Get the command at [param position]. 
-## [br]You can also use [method get] instead.
-func get_command(position:int) -> Resource:
-	if position < collection.size():
-		return collection[position]
-	
-	push_error("get_command: Tried to get an command on a non-existing position: ", position)
-	return null
-
-func get_command_position(command) -> int:
-	return collection.find(command)
-
-func has(value:Command) -> bool:
-	return collection.has(value)
-
-func find(command) -> int:
-	return collection.find(command)
-
-func size() -> int:
-	return collection.size()
-
-func is_empty() -> bool:
-	return collection.is_empty()
-
-func _update_command_owner(command:Command, remove:bool=false) -> void:
-	if not weak_owner: return
-	if typeof(weak_owner.get_ref()) != TYPE_OBJECT: return
-	command.weak_owner = weakref(self)
-
-func _get(property: StringName):
-	if property.is_valid_int():
-		return get_command(int(property))
-
-func _get_property_list() -> Array:
-	var p:Array = []
-	p.append({"name":"collection", "type":TYPE_ARRAY, "usage":PROPERTY_USAGE_NO_EDITOR|PROPERTY_USAGE_SCRIPT_VARIABLE})
-	return p
-
-func _to_string() -> String:
-	var owner:Object
-	if weak_owner: owner = weak_owner.get_ref()
-	return "<CommandCollection:%s>" % owner
-
-# ITERATOR
-var __itr_cnt:int
-func _should_continue() -> bool: return __itr_cnt < collection.size()
-
-func _iter_init(_d) -> bool:
-	__itr_cnt = 0
-	return _should_continue()
-
-func _iter_next(_d) -> bool:
-	__itr_cnt += 1
-	return _should_continue()
-	
-func _iter_get(_d):
-	return collection[__itr_cnt]
+#func _get_iterator_ref() -> Array: return _command_list
