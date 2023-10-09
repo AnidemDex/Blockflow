7c1dbb8cb85a9b85c708f4faf15fda6b75f01272
AnidemDex <anidemdex@github.com>
2023-10-08 20:56:57 -0500

    Restore timeline.gd from d53492c

--- timeline.gd
+++ timeline.gd
@@ -7,35 +7,75 @@ class_name Timeline
 ## This resource only keeps an ordered reference of all commands registered on it.
 ##
 
-var commands:CommandCollection:
+var commands:Array[Command]:
 	set(value):
-		if commands == value: return # Avoid re-assignation
-		if commands:
-			if commands.changed.is_connected(_notify_changed):
-				commands.changed.disconnect(_notify_changed)
-		
-		if value == null:
-			value = CommandCollection.new()
-		
 		commands = value
-		commands.weak_owner = weakref(self)
-		
-		if commands:
-			commands.changed.connect(_notify_changed)
 		_notify_changed()
 	get:
 		return commands
 
-var _bookmarks:Dictionary = {}
-var _command_list:Array = []
-var _branches:Array = []
-var _subcommads:Array = []
+var _bookmarks:Dictionary
 
-func get_command(position:int) -> Command:
-	if position >= _command_list.size():
-		push_error("position >= _command_list.size()")
-		return null
-	return _command_list[position]
+
+## Adds a [Command] to the timeline
+func add_command(command:Command) -> void:
+	if has(command):
+		push_error("add_command: Trying to add an command to the timeline, but the command is already added")
+		return
+	commands.append(command)
+	_notify_changed()
+
+## Insert an [code]Command[/code] at position.
+func insert_command(command:Command, at_position:int) -> void:
+	if has(command):
+		push_error("insert_command: Trying to add an command to the timeline, but the command already exist")
+		return
+	
+	var idx = at_position if at_position > -1 else commands.size()
+	commands.insert(idx, command)
+	
+	_notify_changed()
+
+## Duplicates a [Command] to the timeline
+func duplicate_command(command:Command, to_position:int) -> void:
+	var duplicated = command.duplicate()
+	var idx = to_position if to_position > -1 else commands.size()
+	commands.insert(idx, duplicated)
+	
+	_notify_changed()
+
+## Moves an [code]command[/code] to position.
+func move_command(command, to_position:int) -> void:
+	if !has(command):
+		push_error("move_command: Trying to move an command in the timeline, but the command is not added.")
+		return
+	
+	var old_position:int = get_command_idx(command)
+	if old_position < 0:
+		return
+	
+	to_position = to_position if to_position > -1 else commands.size()
+	if to_position == old_position:
+		emit_changed()
+		return
+	
+	commands.remove_at(old_position)
+	
+	if to_position < 0 or to_position > commands.size():
+		to_position = commands.size()
+	
+	commands.insert(to_position, command)
+	
+	_notify_changed()
+	notify_property_list_changed()
+
+## Get the command at [code]position[/code]
+func get_command(position:int) -> Resource:
+	if position < commands.size():
+		return commands[position]
+	
+	push_error("get_command: Tried to get an command on a non-existing position: ", position)
+	return null
 
 ## Get the command [code]position[/code] from its [code]bookmark[/code]
 func get_command_by_bookmark(bookmark:StringName) -> Resource:
@@ -44,70 +84,40 @@ func get_command_by_bookmark(bookmark:StringName) -> Resource:
 	
 	return _bookmarks.get(bookmark, null)
 
+## Removes an command from the timeline.
+func erase_command(command) -> void:
+	commands.erase(command)
+	_notify_changed()
+
+## Removes an command at [code]position[/code] from the timelin
+func remove_command(position:int) -> void:
+	commands.remove_at(position)
+	_notify_changed()
+
 ## Returns the command position in the timeline.
-func get_command_position(command) -> int:
-	return _command_list.find(command)
-
-func get_command_count() -> int:
-	return _command_list.size()
-
-func is_branch(command:Command) -> bool:
-	return command in _branches
-
-func is_subcommand(command:Command) -> bool:
-	return command in _subcommads
+func get_command_idx(command) -> int:
+	return commands.find(command)
 
 func has(value:Command) -> bool:
 	return commands.has(value)
 
-## Forces an update to contained data.
-## [br]This makes all the contained data:
-## [br]- Update their index.
-## [br]- Update their bookmars.
-## [br]- Update their owners.
-## [br]- Update their structure.
-func update() -> void:
-	_notify_changed()
-
-var _index:int
-func _update_data(from_collection:CommandCollection):
-	if not from_collection: return
-	
-	for command in from_collection:
-		_command_list.append(command)
-		command.index = _index
-		_index += 1
-		# branches goes first
-		if command.branches:
-			_update_data(command.branches)
-			_branches.append_array(command.branches.collection)
-		
-		if command.commands:
-			_update_data(command.commands)
-			_subcommads.append_array(command.commands.collection)
-		
+func update_bookmarks() -> void:
+	for command in commands:
 		if not command.bookmark.is_empty():
 			_bookmarks[command.bookmark] = command
-		
-		command.weak_timeline = weakref(self)
 
-var _updating_data:bool = false
+func update_indexes() -> void:
+	# This probably will cause performance issues
+	for command_idx in commands.size():
+		commands[command_idx].index = command_idx
+
 func _notify_changed() -> void:
-	if _updating_data: return
-	_updating_data = true
-	_index = 0
-	_command_list = []
-	_update_data(commands)
-	_updating_data = false
+	update_bookmarks()
+	update_indexes()
 	emit_changed()
-
-func _to_string() -> String:
-	return "<Timeline#%s>" % get_instance_id()
+	pass
 
 func _get_property_list() -> Array:
 	var p:Array = []
-	p.append({"name":"commands", "type":TYPE_OBJECT, "usage":PROPERTY_USAGE_NO_EDITOR})
+	p.append({"name":"commands", "type":TYPE_ARRAY, "usage":PROPERTY_USAGE_NO_EDITOR|PROPERTY_USAGE_SCRIPT_VARIABLE})
 	return p
-
-func _init() -> void:
-	commands = CommandCollection.new()
