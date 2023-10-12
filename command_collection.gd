234aa57b333e636e81a0e5ba8d83714671925c63
AnidemDex <anidemdex@github.com>
2023-10-12 10:39:06 -0500

    Update CommandCollection way of updating data.
    Add CommandCollection._update_command_data() as recursive helper to set command data.

--- command_collection.gd
+++ command_collection.gd
@@ -10,6 +10,7 @@ var _bookmarks:Dictionary = {}
 var _command_list:Array = []
 var _branches:Array = []
 var _subcommads:Array = []
+var _command_data:Dictionary = {}
 
 func get_command(position:int) -> Command:
 	if position >= _command_list.size():
@@ -31,6 +32,36 @@ func get_command_absolute_position(command) -> int:
 func get_command_count() -> int:
 	return _command_list.size()
 
+## Return -1 if the position doesn't exist or there's no
+## possible next command position.
+func get_next_command_position_according(position:int) -> int:
+	if position >= get_command_count():
+		return -1
+	if position + 1 >= get_command_count():
+		return -1
+	
+	var command:Command = get_command(position)
+	var next_position = position + 1
+	if command.can_hold_commads and not command.commands.is_empty():
+		next_position = command.commands.get_command(0).index
+		return next_position
+	
+	# Oh mother of all ducks, I really need another
+	# way of doing branches
+	var next_command:Command = get_command(position + 1)
+	while is_instance_of(next_command, Command.Branch):
+		next_position = \
+		_command_data[next_command.get_command_owner()]["total_branch_count"] + 1
+		if next_position < get_command_count():
+			next_command = get_command(next_position)
+		else:
+			next_command = null
+			next_position = -1
+			break
+		
+	return next_position
+
+
 func is_branch(command:Command) -> bool:
 	return command in _branches
 
@@ -50,23 +81,51 @@ var _index:int
 func _update_data(from_collection:Array):
 	if from_collection.is_empty(): return
 	
+	_bookmarks.clear()
 	for command in from_collection:
-		_command_list.append(command)
-		command.index = _index
-		_index += 1
-		# branches goes first
-		if command.branches:
-			_update_data(command.branches.collection)
-			_branches.append_array(command.branches.collection)
-		
-		if command.commands:
-			_update_data(command.commands.collection)
-			_subcommads.append_array(command.commands.collection)
+		_update_command_data(command)
 		
 		if not command.bookmark.is_empty():
 			_bookmarks[command.bookmark] = command
 		
 		command.weak_collection = weakref(self)
+
+func _update_command_data(command:Command) -> void:
+	_command_data[command] = {}
+	_command_list.append(command)
+	command.index = _index
+	_index += 1
+	
+	# See this trick here? 
+	# I hate it
+	# and you will
+	var owner:Command = command.get_command_owner()
+	if owner:
+		var i = 0
+		var top_owner
+		# What we try is to count, from bottom
+		# how far are we from our top-most branch creator
+		while owner != null:
+			if owner:
+				top_owner = owner
+				if owner.has_branches():
+					_command_data[owner]["total_branch_count"] += 1
+			i += 1
+			owner = owner.get_command_owner()
+		
+	
+	if not command.bookmark.is_empty():
+		_bookmarks[command.bookmark] = command
+	
+	# Branches goes first
+	if command.branches:
+		_command_data[command]["total_branch_count"] = 0
+		for branch in command.branches:
+			_update_command_data(branch)
+	
+	if command.commands:
+		for subcommand in command.commands:
+			_update_command_data(subcommand)
 
 
 var _updating_data:bool = false
