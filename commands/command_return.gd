ad796755696a6e557eadc32a7648ecfc7e9f1246
Dex <dav.medrin@gmail.com>
2023-10-30 18:03:38 -0500

    Update command_return.gd
    Remove unused code

--- commands/command_return.gd
+++ commands/command_return.gd
@@ -19,18 +19,3 @@ func _get_hint() -> String:
 
 func _get_icon() -> Texture:
 	return load("res://addons/blockflow/icons/return.svg")
-
-# stop at end by default
-func _property_can_revert(property: StringName) -> bool:
-	if property == "continue_at_end":
-		return true
-	return false
-
-func _property_get_revert(property: StringName):
-	if property == "continue_at_end":
-		return false
-	return null
-
-func _init() -> void:
-	super()
-	continue_at_end = false
