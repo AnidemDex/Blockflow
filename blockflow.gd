87ddc1450d4a9c8a86df385cd9e63a2747a584c0
AnidemDex <anidemdex@github.com>
2023-11-03 21:06:40 -0500

    Update Blockflow script.
    Add a function to generate the complete command list of any collection.

--- blockflow.gd
+++ blockflow.gd
@@ -1,4 +1,4 @@
-
+@tool
 ## Modifying values here requires a plugin reload after save.
 
 const DEFAULT_COMMAND_PATHS = [
@@ -17,3 +17,56 @@ const PROJECT_SETTING_DEFAULT_COMMANDS =\
 "blockflow/settings/commands/default_commands"
 const PROJECT_SETTING_CUSTOM_COMMANDS =\
 "blockflow/settings/commands/custom_commands"
+
+const Utils = preload("res://addons/blockflow/core/utils.gd")
+
+# Made to ensure that classes are loaded before class_name populates editor
+const CollectionClass = preload("res://addons/blockflow/collection.gd")
+const CommandCollectionClass = preload("res://addons/blockflow/command_collection.gd")
+const CommandClass = preload("res://addons/blockflow/commands/command.gd")
+
+class CollectionData:
+	var main_collection:CollectionClass
+	var command_list:Array[CommandClass]
+	var bookmarks:Dictionary
+
+static func generate_tree(collection:Collection) -> CollectionData:
+#	if collection.is_updating_data: return
+	collection.is_updating_data = true
+	
+	var data := CollectionData.new()
+	
+	var command_pt:CommandClass
+	
+	if collection.is_empty():
+		return data
+	
+	var command_list:Array[CommandClass] = []
+	var owner:Collection
+	command_pt = collection.collection[0]
+	
+	for command in collection.collection:
+		_recursive_add(command, command_list)
+	
+	var position:int = 0
+	var bookmarks := {}
+	for command_position in command_list.size():
+		command_pt = command_list[command_position]
+		command_pt.position = command_position
+		command_pt.weak_collection = weakref(collection as CommandCollectionClass)
+	
+	data.command_list = command_list
+	data.bookmarks = bookmarks
+	data.main_collection = collection
+	
+	collection.set("data", data)
+	
+	collection.is_updating_data = false
+	
+	return data
+
+static func _recursive_add(command, to) -> void:
+	to.append(command)
+	for subcommand in command:
+		_recursive_add(subcommand, to)
+	
