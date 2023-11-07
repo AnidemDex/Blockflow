7f3ff987ffa248c096fea8a136b3250ae1f7d3be
AnidemDex <anidemdex@github.com>
2023-11-07 10:29:56 -0500

    Update CommandCondition.
    Update branch condition evaluation
    Remove unused code from condition command.
    Update true and false branches branch names
    Update false branch condition is true condition.

--- commands/branch_false.gd
+++ commands/branch_false.gd
@@ -1,5 +1,10 @@
 @tool
 extends "res://addons/blockflow/commands/branch.gd"
 
+func _condition_is_true() -> bool: return not super()
+
 func _get_icon() -> Texture:
 	return load("res://addons/blockflow/icons/false.svg")
+
+func _init() -> void:
+	branch_name = "is False"
--- commands/branch_true.gd
+++ commands/branch_true.gd
@@ -3,3 +3,6 @@ extends "res://addons/blockflow/commands/branch.gd"
 
 func _get_icon() -> Texture:
 	return load("res://addons/blockflow/icons/true.svg")
+
+func _init() -> void:
+	branch_name = "is True"
