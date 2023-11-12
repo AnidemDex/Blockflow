5d7883333a64eed436f4d674dadb0a085b6177b9
AnidemDex <anidemdex@github.com>
2023-11-12 15:19:38 -0500

    Update Blockflow.Utils
    Add a method to get the data Dictionary equivalent from a blockflow object.

--- core/utils.gd
+++ core/utils.gd
@@ -18,3 +18,58 @@ static func evaluate(input:String, global:Object=null, locals:Dictionary={}, _sh
 			return input
 		
 	return _evaluated_value
+
+static func get_object_data(object:Object) -> Dictionary:
+	const CollectionClass = preload("res://addons/blockflow/collection.gd")
+	const ProcessorClass = preload("res://addons/blockflow/command_processor.gd")
+	const CommandClass = preload("res://addons/blockflow/commands/command.gd")
+	const CCollectionClass = preload("res://addons/blockflow/command_collection.gd")
+	
+	var data:Dictionary = {}
+	if not is_instance_valid(object): return data
+	
+	data[&"id"] = object.get_instance_id()
+	if object is Node:
+		data[&"path"] = object.get_path()
+		data[&"name"] = object.name
+		if is_instance_of(object, ProcessorClass):
+			data[&"main_collection"] = get_object_data(object.main_collection)
+			data[&"current_collection"] = get_object_data(object.current_collection)
+			data[&"current_command"] = get_object_data(object.current_command)
+		return data
+	
+	if is_instance_of(object, CollectionClass):
+		data[&"class"] = &"Collection"
+		if is_instance_of(object, CommandClass):
+			data[&"class"] = &"Command"
+			data[&"name"] = object.command_name
+			data[&"index"] = object.index
+			data[&"position"] = object.position
+			if is_instance_valid(object.command_manager):
+				data[&"processor"] = object.command_manager.get_instance_id()
+		
+		if is_instance_of(object, CCollectionClass):
+			data[&"class"] = &"CommandCollection"
+			data[&"name"] = object.resource_path
+			data[&"bookmarks"] = {}
+			for bookmark in object._bookmarks.keys():
+				var command = object._bookmarks[bookmark]
+				data[&"bookmarks"][bookmark].append(command.get_instance_id())
+			data[&"command_list"] = []
+			for command in object._command_list:
+				data[&"command_list"].append(get_object_data(command))
+		
+		data[&"collection"] = []
+		for command in object.collection:
+			data[&"collection"].append(get_object_data(command))
+		return data
+	
+	return data
+
+static func obj_to_str(object:Object) -> void:
+	push_error("Not implemented")
+	return
+
+static func str_to_obj(string:String) -> void:
+	push_error("Not implemented")
+	return
