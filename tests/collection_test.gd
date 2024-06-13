13eafd12a327db4a0c06c3e59ee3ad8365b0b3ef
AnidemDex <dav.medrin@gmail.com>
2024-06-12 20:20:12 -0500

    Create Collection test suit

---
+++ tests/collection_test.gd
@@ -0,0 +1,83 @@
+# GdUnit generated TestSuite
+class_name CollectionTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source = 'res://addons/blockflow/collection.gd'
+
+
+func test_add() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_insert() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_copy() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_move() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_erase() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_remove() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_clear() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_get_command() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_get_last_command() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_get_command_position() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_get_duplicated() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_has() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_find() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_size() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
+
+
+func test_is_empty() -> void:
+	# remove this line and complete your test
+	assert_not_yet_implemented()
