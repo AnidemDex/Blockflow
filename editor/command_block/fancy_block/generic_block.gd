079594602d9ad3b78a7a5812c8de83da26753a47
AnidemDex <dav.medrin@gmail.com>
2024-10-31 22:03:45 -0500

    Add Generic Block scene.
    Add a generic block scene to demonstrate how to create custom scenes for command blocks

---
+++ editor/command_block/fancy_block/generic_block.gd
@@ -0,0 +1,32 @@
+@tool
+extends "res://addons/blockflow/editor/command_block/fancy_block/block.gd"
+
+@onready var label_hint:Label = get_node_or_null(^"0/LabelHint") as Label
+@onready var btn_collapse:Button = get_node_or_null(^"1/BtnCollapse") as Button
+
+func _update_block() -> void:
+	super()
+	if is_instance_valid(label_hint):
+		label_hint.text = command.command_hint
+	
+	if is_instance_valid(btn_collapse):
+		_update_toggle_icon()
+		btn_collapse.visible = not command.collection.is_empty()
+
+func _update_toggle_icon() -> void:
+	if btn_collapse.button_pressed:
+		btn_collapse.icon = get_theme_icon("CodeFoldedRightArrow", "EditorIcons")
+	else:
+		btn_collapse.icon = get_theme_icon("CodeFoldDownArrow", "EditorIcons")
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_VISIBILITY_CHANGED:
+		for subcommand in command.collection:
+			if is_instance_valid(subcommand.editor_block):
+				subcommand.editor_block.visible = visible
+
+func _on_btn_collapse_toggled(toggled_on: bool) -> void:
+	_update_toggle_icon()
+	for subcommand in command.collection:
+		if is_instance_valid(subcommand.editor_block):
+			subcommand.editor_block.visible = not toggled_on
